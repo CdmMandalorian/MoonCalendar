f12065af20f1468efba8bd828f9301c48f95156b
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-10-14 13:52:52 -0700

    Assets folder holding Moon phases images

--- NightMoonCalendar/src/App.js
+++ NightMoonCalendar/src/App.js
@@ -3,15 +3,19 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
 import './Style.css';
+import Moon from 'components/Moon/Moon';
 
 function App() {
   const [date, setDate] = useState(new Date());
-
+  
   return (
-    <div className='app'>
-      <h1 className='text-center'>Night Moon Calendar</h1>
+    <div className='NightMoonCalendar'>
+      <h1 className='h1-Heading'>Night Moon Calendar</h1>
       <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+        <Calendar 
+          onChange={setDate} value={date}  > 
+            <Moon></Moon>
+        </Calendar>
       </div>
       <p className='text-center'>
         <span className='bold'>Selected Date:</span>{' '}
---
+++ NightMoonCalendar/src/components/Moon/Moon.js
@@ -0,0 +1,14 @@
+const moon = require('celestial-moon');
+
+function Moon() {
+
+    const phase = moon.calculate(2020, 11, 10, 9, 30, 0, 0);
+
+    return(
+
+
+    );
+}
+
+
+export default moon;
