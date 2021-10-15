68fe2e03b43223a36a79bbceb19c99b79eca1524
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-10-15 07:30:38 -0700

    editing public index.html file, and retraced steps back to a working calendar

--- NightMoonCalendar/src/App.js
+++ NightMoonCalendar/src/App.js
@@ -3,18 +3,23 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
 import './Style.css';
-import Moon from 'components/Moon/Moon';
+import Moon from './components/Moon';
 
 function App() {
+ 
+  const Phase = Moon.calculate(2020, 11, 10, 9, 30, 0, 0);
   const [date, setDate] = useState(new Date());
   
+
   return (
     <div className='NightMoonCalendar'>
-      <h1 className='h1-Heading'>Night Moon Calendar</h1>
+      <h1 className='h1-Heading'>NightMoon Calendar</h1>
       <div className='calendar-container'>
         <Calendar 
           onChange={setDate} value={date}  > 
-            <Moon></Moon>
+            <Moon>
+              <Phase></Phase>
+            </Moon>
         </Calendar>
       </div>
       <p className='text-center'>
