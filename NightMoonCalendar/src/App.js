0addf96f7477958180efc981d75780b2fe711e97
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-11-08 09:24:14 -0800

    Set React mobiscroll for Calendar Function

--- NightMoonCalendar/src/App.js
+++ NightMoonCalendar/src/App.js
@@ -1,33 +1,52 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import './App.css';
-import './Style.css';
-import Moon from './components/Moon';
+import React from 'react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+import { Eventcalendar, getJson } from '@mobiscroll/react';
 
 function App() {
- 
-  const Phase = Moon.calculate(2020, 11, 10, 9, 30, 0, 0);
-  const [date, setDate] = useState(new Date());
-  
 
-  return (
-    <div className='NightMoonCalendar'>
-      <h1 className='h1-Heading'>NightMoon Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar 
-          onChange={setDate} value={date}  > 
-            <Moon>
-              <Phase></Phase>
-            </Moon>
-        </Calendar>
-      </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
-    </div>
-  );
+    const [myEvents, setEvents] = React.useState([]);
+
+    React.useEffect(() => {
+        getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
+            setEvents(events);
+        }, 'jsonp');
+    }, []);
+    
+    const responsive = React.useMemo(() => {
+        return {
+            xsmall: {
+                view: {
+                    calendar: {
+                        type: 'week',
+                    },
+                    agenda: {
+                        type: 'day'
+                    }
+                }
+            },
+            custom: { // Custom breakpoint
+                breakpoint: 600,
+                view: {
+                    calendar: {
+                        labels: true
+                    }
+                }
+            }
+        };
+    }, []);
+
+    return (
+        <Eventcalendar
+            theme="ios" 
+            themeVariant="light"
+            clickToCreate={false}
+            dragToCreate={false}
+            dragToMove={false}
+            dragToResize={false}
+            data={myEvents}
+            responsive={responsive}
+       />
+    ); 
 }
 
 export default App;
